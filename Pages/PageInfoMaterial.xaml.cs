761bee48aed95c36e69fc903d7336c0e94163fe5
4e6yPaLIIKa228 <alexander_pushkar@mail.ru>
2022-03-27 15:13:42 +0300

    test
    test

---
+++ Pages/PageInfoMaterial.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Demo334.DataBase;
+using Demo334.Pages;
+
+
+namespace Demo334.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageInfoMaterial.xaml
+    /// </summary>
+    public partial class PageInfoMaterial : Page
+    {
+        public PageInfoMaterial()
+        {
+            InitializeComponent();
+            UpdateData();
+            //DispatcherTimer timer = new DispatcherTimer();
+            //timer.Interval = TimeSpan.FromSeconds(1);
+            //timer.Tick += UpdateData;
+            //timer.Start();
+
+        }
+
+        public void UpdateData()
+        {
+            var AllMaterials = Connect.conn.Material.ToList();
+            ListMaterial.ItemsSource = AllMaterials;
+            //ListMaterial.ItemsSource = Connect.conn.Material.Where(x => x.Title.StartsWith(TxtSearch.Text) || x.Description.StartsWith(TxtSearch.Text)).ToList();
+        }
+
+        private void TxtSearch_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            ListMaterial.ItemsSource = Connect.conn.Material.Where(x => x.Title.StartsWith(TxtSearch.Text) || x.Description.StartsWith(TxtSearch.Text)).ToList();
+        }
+    }
+}
